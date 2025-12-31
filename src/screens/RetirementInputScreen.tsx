6d8ca812e0e1a3e004236b5e590b52d659fdbaad
Tarun Singh Thakur <admin@finthinkhub.com>
2025-12-31 15:34:39 +0530

    chore: add retirement flow screen placeholders

---
+++ src/screens/RetirementInputScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function RetirementInputScreen() {
+    return (
+        <View>
+            <Text>Retirement Input Screen</Text>
+        </View>
+    );
+}
